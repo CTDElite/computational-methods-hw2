2db7e48f1d11383b1b71f6156be8521dad6cf005
itegulov <ditegulov@gmail.com>
2016-03-29 13:32:59 +0300

    Fix gradient_descent and add test to main

--- headers/gradient_descent_solver.h
+++ headers/gradient_descent_solver.h
@@ -6,22 +6,34 @@
 #define COMPUTATION_METHODS_HW2_NG_GRADIENT_DESCENT_SOLVER_H
 
 #include "iterative_solver.h"
+#include <memory>
+#include <iostream>
+
 template <class T>
 class gradient_descent_solver : public iterative_solver<T> {
 public:
     matrix<T> solve(const matrix<T> &coefficients, const matrix<T> &free,
                     const matrix<T> &initial_approx, size_t iterations) {
-        matrix<T> xp(initial_approx);
-        matrix<T> fp(coefficients * xp - free);
-        const matrix<T> W = coefficients.transpose();
+        std::unique_ptr<matrix<T>> xp = std::make_unique<matrix<T>>(initial_approx);
+        std::unique_ptr<matrix<T>> fp = std::make_unique<matrix<T>>(coefficients * (*xp) - free);
+        matrix<T> W = coefficients.transpose();
+
+        std::cout << *fp << std::endl;
+        std::cout << std::endl;
+        std::cout << *xp << std::endl;
+        std::cout << std::endl << std::endl;
 
         for (size_t iter = 0; iter < iterations; iter++) {
-            matrix<T> h = (coefficients * W * fp);
-            T nup = (fp.transpose() * h).getAsT() / (h * h.transpose()).getAsT();
-            xp = xp - (W * fp) * nup;
-            fp = coefficients * xp - free;
+            matrix<T> h(coefficients * W * (*fp));
+            T nup = (fp->transpose() * h).getAsT() / (h.transpose() * h).getAsT();
+            xp = std::make_unique<matrix<T>>((*xp) - (W * (*fp)) * nup);
+            fp = std::make_unique<matrix<T>>(coefficients * (*xp) - free);
+            std::cout << *fp << std::endl;
+            std::cout << std::endl;
+            std::cout << *xp << std::endl;
+            std::cout << std::endl << std::endl;
         }
-        return xp;
+        return *xp;
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,26 @@
 //
 #include <iostream>
 #include <vector>
-#include <gmpxx.h>
 #include "matrix.h"
 #include "gauss_solver.h"
+#include <gradient_descent_solver.h>
+#include <memory>
 
 using namespace std;
 
 int main() {
-    matrix<mpq_class> m ({
-                       {1, 2, 3},
-                       {1, 2, 1},
-                       {1, 1, 1},
+    unique_ptr<iterative_solver<double>> solver2 = make_unique<gradient_descent_solver<double>>();
+    matrix<double> m2({
+                              {1, 1},
+                              {1, 3}
                       });
-    matrix<mpq_class> fr({
-                                 {1},
-                                 {2},
-                                 {3},
-                         });
-    gauss_solver<mpq_class> solver;
-    cout << m * solver.solve(m, fr);
+    matrix<double> fr2({
+                               {-3},
+                               {-7}
+                      });
+    matrix<double> init({
+                                {1},
+                                {1}
+                        });
+    cout << solver2->solve(m2, fr2, init, 3);
 }
